5ab67400eba1aa3369a25a5a9f1dfa5b24f1a508
Mridul Anand <mridulanand159159@gmail.com>
2025-08-24 08:48:11 +0530

    Add files via upload

---
+++ Porfolio/src/config/data.jsx
@@ -0,0 +1,69 @@
+const personalData = {
+    // Home Section
+    name: "Mridul Anand",
+    title: ["Full Stack Developer", "Web Designer", "UI/UX Designer"],
+    homeImg: "images/dp.jpg",
+    email: "mridulanand159159@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/mridul-anand-025415324/",
+    github: "https://github.com/mridul159159",
+    twitter: "https://x.com/MridulAnan9331?t=RKUojVDO-i00loPQ2OOMpw&s=09",
+    instagram: "https://www.instagram.com/marco_k_ing?igsh=MThjYmxubGVndmIzdQ==&utm_source=ig_contact_invite",
+    mail: "mridulanand159.com",
+    resume: "resume/resume.pdf",
+
+    // About Section
+    aboutImg: "images/dp.jpg",
+    aboutTitle: "Full Stack Developer",
+    aboutText: "Hi, I'm Mridul Anand, a frontend developer based in India. I have a passion for web development and I'm currently learning React, HTML, CSS, and JavaScript. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt laudantium quibusdam voluptate doloribus, hic asperiores at, consequatur assumenda aut porro voluptatibus aspernatur, voluptatem itaque corrupti! Exercitationem doloribus ullam totam? Voluptate! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas iusto porro pariatur amet. Ut aperiam perferendis veritatis recusandae, consectetur sunt fuga hic similique accusantium, quae dicta aliquid adipisci minus fugiat?",
+
+    // Services Section
+    service1Title: "Web Development",
+    service1Text: "Experienced in building responsive and user-friendly websites using HTML, CSS, and JavaScript.",
+    service2Title: "Graphic Design",
+    service2Text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+    service3Title: "Android Development",
+    service3Text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+
+    // Education Section
+    edu3Year: "2016 - 2017",
+    edu3Course: "HighSchool",
+    edu3Stream: "Science",
+    edu3College: "S Devi I C Madhuban Mau",
+    edu3Grade: "A+",
+
+    edu1Year: "2018 - 2021",
+    edu1Course: "Diploma",
+    edu1Stream: "Mechanical Engineering",
+    edu1College: "CSJM Govt Plytechnic Balrampur",
+    edu1Grade: "A+",
+
+    edu2Year: "2023 - 2026",
+    edu2Course: "B.Tech",
+    edu2Stream: "Computer Science and Engineering",
+    edu2College: "RRIMT",
+    edu2Grade: "A+",
+
+    
+    // Portfolio Section
+    project1Title: "Vidhub Downloader",
+    project1Img: "images/project1.jpeg",
+    project1Text: "A Python-based video downloading tool using the PyTube library to fetch and save videos from YouTube.Built for seamless integration and easy media access in content portfolios.",
+    project2Title: "Unit Converter",
+    project2Img: "images/project2.png",
+    project2Text: " A responsive web-based unit converter built with HTML, CSS, and JavaScript.Supports real-time conversions across multiple measurement units with a user-friendly interface.",
+    project3Title: "Project 3",
+    project3Img: "images/project3.png",
+    project3Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+    project4Title: "Amazone Clone",
+    project4Img: "images/project4.webp",
+    project4Text: "A fully responsive e-commerce website clone of Amazon built using HTML, CSS, JavaScript, and ReactJS.Features include product listings, search functionality, and an interactive user interface.",
+    project5Title: "Project 5",
+    project5Img: "images/project5.jpg",
+    project5Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+    project6Title: "Project 6",
+    project6Img: "images/project6.webp",
+    project6Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+
+};
+
+export default personalData;
